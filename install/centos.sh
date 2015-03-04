991684c9867029bbe061b9152d1da22fafad6eed
Tim Kuminecz <tkuminecz@synacor.com>
2015-03-04 00:02:56 -0500

    Update centos install script

--- install/centos.sh
+++ install/centos.sh
@@ -1,7 +1,3 @@
-# install env files
-../symlink.sh
-. ~/.bashrc
-
 # install nodejs and npm
 sudo yum install -y epel-release
 sudo yum install -y nodejs
@@ -12,3 +8,13 @@ sudo timedatectl set-timezone America/New_York
 sudo yum install -y ntp
 sudo systemctl start ntpd
 sudo systemctl enable ntpd
+
+# install ruby
+sudo yum install -y ruby
+
+# install cpan
+sudo yum install -y cpan
+
+# install env files
+../symlink.sh
+. ~/.bashrc
